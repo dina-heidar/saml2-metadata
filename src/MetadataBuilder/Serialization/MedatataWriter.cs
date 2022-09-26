c5589092f5ceac86afcd997f1649053a405af316
dheida1 <dinaheidar13@gmail.com>
2022-09-25 21:26:33 -0500

    base EntityDescriptorType metadata working

--- src/MetadataBuilder/Serialization/MedatataWriter.cs
+++ src/MetadataBuilder/Serialization/MedatataWriter.cs
@@ -75,53 +75,53 @@ namespace Saml.MetadataBuilder
 
         public async Task<XmlDocument> Output(EntityDescriptor entityDescriptor)
         {
-            UiInfo ui = new UiInfo
-            {
-                Description = new LocalizedName { Language = "en-US", Value = "A test OTS site" },
-                DisplayName = new LocalizedName { Language = "en-US", Value = "OTS" },
-                Keywords = new Keyword { Language = "en-US", Values = new[] { "ots", "saml", "hello" } }
-            };
-            //};
+        //    UiInfo ui = new UiInfo
+        //    {
+        //        Description = new LocalizedName { Language = "en-US", Value = "A test OTS site" },
+        //        DisplayName = new LocalizedName { Language = "en-US", Value = "OTS" },
+        //        Keywords = new Keyword { Language = "en-US", Values = new[] { "ots", "saml", "hello" } }
+        //    };
+       
 
-            var uIInfoType =
-                new UIInfoType
-                {
-                    Items = new object[] {
-                        new localizedNameType { lang = ui.DisplayName.Language, Value = ui.DisplayName.Value },
-                        new localizedNameType { lang = "fr", Value ="OTTSe" },
-                        new localizedNameType { lang = "fr", Value ="OTS us in LA" }
-                    },
-                    ItemsElementName = new ItemsChoiceType8[] {
-                        ItemsChoiceType8.@DisplayName,
-                        ItemsChoiceType8.@DisplayName,
-                        ItemsChoiceType8.@Description
-                    }
-                }; 
+        //var uIInfoType =
+        //    new UIInfoType
+        //    {
+        //        Items = new object[] {
+        //                new localizedNameType { lang = ui.DisplayName.Language, Value = ui.DisplayName.Value },
+        //                new localizedNameType { lang = "fr", Value ="OTTSe" },
+        //                new localizedNameType { lang = "fr", Value ="OTS us in LA" }
+        //        },
+        //        ItemsElementName = new ItemsChoiceType8[] {
+        //                ItemsChoiceType8.@DisplayName,
+        //                ItemsChoiceType8.@DisplayName,
+        //                ItemsChoiceType8.@Description
+        //        }
+        //    };
 
-            string xmlTemplate2 = string.Empty;
-            XmlSerializer ser = new XmlSerializer(typeof(UIInfoType));
-            XmlElement myElement =
-            new XmlDocument().CreateElement("UIInfo");
-            //myElement.InnerText = "Hello World";
-            using (MemoryStream memStm = new MemoryStream())
-            {
-                ser.Serialize(memStm, uIInfoType);
-                memStm.Position = 0;
-                xmlTemplate2 = new StreamReader(memStm).ReadToEnd();
-            }
-            XmlDocument xmlDoc1 = new XmlDocument();
-            xmlDoc1.LoadXml(xmlTemplate2);
-            XmlElement element = xmlDoc1.DocumentElement;//.SelectSingleNode("//UIInfo");//xmlDoc1.DocumentElement.SelectNodes("//*");
+        //string xmlTemplate2 = string.Empty;
+        //    XmlSerializer ser = new XmlSerializer(typeof(UIInfoType));
+        //    XmlElement myElement =
+        //    new XmlDocument().CreateElement("UIInfo");
+        //    //myElement.InnerText = "Hello World";
+        //    using (MemoryStream memStm = new MemoryStream())
+        //    {
+        //        ser.Serialize(memStm, uIInfoType);
+        //        memStm.Position = 0;
+        //        xmlTemplate2 = new StreamReader(memStm).ReadToEnd();
+        //    }
+        //    XmlDocument xmlDoc1 = new XmlDocument();
+        //    xmlDoc1.LoadXml(xmlTemplate2);
+        //    XmlElement element = xmlDoc1.DocumentElement;//.SelectSingleNode("//UIInfo");//xmlDoc1.DocumentElement.SelectNodes("//*");
 
-            XmlElement[] elementarray = new XmlElement[] { element };//nodes.Cast<XmlElement>();
+        //    XmlElement[] elementarray = new XmlElement[] { element };//nodes.Cast<XmlElement>();
 
 
             var entityDescriptorType = _metadataMapper.MapEntity(entityDescriptor);
 
-            entityDescriptorType.Extensions = new ExtensionsType
-            {
-                Any = elementarray 
-            };
+            //entityDescriptorType.Extensions = new ExtensionsType
+            //{
+            //    Any = elementarray 
+            //};
 
             string xmlTemplate = string.Empty;
             XmlSerializer xmlSerializer = new XmlSerializer(typeof(EntityDescriptorType));
